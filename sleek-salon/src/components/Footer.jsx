b91c4f8ab0eadb2fd0fbc096ed71757cc167570a
Jonathan <jonathanafugwobi1@gmail.com>
2025-08-04 22:15:03 +0100

    Add Footer and Header Components

---
+++ sleek-salon/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+    return (
+        // Footer Section Design
+
+        <footer className="footer" id="footer">
+            <div className="footer-container">
+                <p> &copy; <span id="year"></span>, Sleek Unisex Salon || All rights reserved.</p>
+                <a href="#home"><i className='bx bx-up-arrow-alt'></i></a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ sleek-salon/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Nav from "../components/Nav";
+
+function Header() {
+    return (
+        //  Header Section
+
+        <header className="header">
+            <a href="#" className="logo">Sleek</a>
+
+            <Nav />
+        </header>
+    )
+}
+
+export default Header;
