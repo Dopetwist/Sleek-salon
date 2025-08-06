15a5319b50a03619e5269bf30fa42fc2c7b35230
Jonathan <jonathanafugwobi1@gmail.com>
2025-08-06 22:14:30 +0100

    refactor: Imported products array and mapped through each item directly on the Product component

--- sleek-salon/src/App.jsx
+++ sleek-salon/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import Header from './components/Header.jsx';
 import Home from './components/Home.jsx';
 import About from './components/About.jsx';
@@ -7,18 +6,7 @@ import Gallery from './components/Gallery.jsx';
 import Contact from './components/Contact.jsx';
 import Footer from './components/Footer.jsx';
 import Product from './components/Product.jsx';
-import { productsPhotos } from "../index.js";
 
-function displayProduct(item) {
-  <Product 
-  id={item.id}
-  key={item.id}
-  image={item.img}
-  title={item.title}
-  description={item.description}
-  price={item.price}
-  />
-}
 
 function App() {
 
--- sleek-salon/src/components/Product.jsx
+++ sleek-salon/src/components/Product.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { productsPhotos } from "../../index";
 
-function Product(props) {
+function Product() {
     return (
         // Product section design
 
@@ -8,20 +9,22 @@ function Product(props) {
             <h2 class="heading">Hair <span>Products</span></h2>
 
             <div class="grid-container">
-                <div class="grid-box">
-                    <div class="prod-img">
-                        <img src={props.image} alt="" />
-                    </div>
+                {productsPhotos.map((item) => (
+                    <div class="grid-box">
+                        <div class="prod-img">
+                            <img src={item.img} alt="Product Image" />
+                        </div>
 
-                    <div class="description">
-                        <h3>{props.title}</h3>
-                        <p>{props.description}</p>
-                        <div class="sub">
-                            <p class="price">{props.price}</p>
-                            <button>Order</button>
+                        <div class="description">
+                            <h3>{item.title}</h3>
+                            <p>{item.description}</p>
+                            <div class="sub">
+                                <p class="price">{item.price}</p>
+                                <button>Order</button>
+                            </div>
                         </div>
                     </div>
-                </div>
+                ))}
             </div>
         </section>
     )
