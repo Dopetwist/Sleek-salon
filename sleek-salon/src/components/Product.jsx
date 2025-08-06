f7479dd99cbbe4576921cbccfa707b0f64b4a582
Jonathan <jonathanafugwobi1@gmail.com>
2025-08-06 22:40:47 +0100

    fix: Change all class to className in Product Component

--- sleek-salon/src/components/Product.jsx
+++ sleek-salon/src/components/Product.jsx
@@ -5,21 +5,21 @@ function Product() {
     return (
         // Product section design
 
-        <section class="products" id="products">
-            <h2 class="heading">Hair <span>Products</span></h2>
+        <section className="products" id="products">
+            <h2 className="heading">Hair <span>Products</span></h2>
 
-            <div class="grid-container">
+            <div className="grid-container">
                 {productsPhotos.map((item) => (
-                    <div class="grid-box">
-                        <div class="prod-img">
+                    <div className="grid-box">
+                        <div className="prod-img">
                             <img src={item.img} alt="Product Image" />
                         </div>
 
-                        <div class="description">
+                        <div className="description">
                             <h3>{item.title}</h3>
                             <p>{item.description}</p>
-                            <div class="sub">
-                                <p class="price">{item.price}</p>
+                            <div className="sub">
+                                <p className="price">{item.price}</p>
                                 <button>Order</button>
                             </div>
                         </div>
