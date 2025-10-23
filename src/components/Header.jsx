60c6145b91d2a61d987947ebb69f5eb6ab99347e
Jonathan <jonathanafugwobi1@gmail.com>
2025-10-23 23:40:28 +0100

    Add hooks to dynamically handle menu icon toggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,29 @@
 
+import { useEffect, useState } from "react";
 import Nav from "../components/Nav";
 import Icons from "./Icons";
 
 function Header() {
+
+    const [ open, setOpen ] = useState(false);
+
+    useEffect(() => {
+
+    }, []);
+    
+
     return (
         //  Header Section
 
         <header className="header">
             <a href="#" className="logo">Sleek</a>
             
-            <Icons.TextAlignJustify id="menu-icon" />
+            <div id="menu-con"
+            onClick={() => {setOpen(!open)}}
+            >
+                { open ? <Icons.X id="menu-icon" />
+                    : <Icons.TextAlignJustify id="menu-icon" /> }
+            </div>
 
             <Nav />
         </header>
