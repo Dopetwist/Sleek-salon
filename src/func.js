343ed52ec5fa01d40c0456cdbbd765f64f24c939
Jonathan <jonathanafugwobi1@gmail.com>
2025-10-25 19:14:17 +0100

    Document code

--- src/func.js
+++ src/func.js
@@ -3,7 +3,7 @@ const currentYear = new Date().getFullYear();
 
 export { currentYear };
 
-
+// Stick Header on scroll
 window.addEventListener("scroll", () => {
         if (scrollY > 0) {
             document.getElementById("header").style.position = "fixed";
