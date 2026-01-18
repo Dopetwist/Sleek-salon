4c61f8be6a1d7d6b1167c5eeb05a284e83ab18cc
Jonathan <jonathanafugwobi1@gmail.com>
2026-01-18 01:35:49 +0100

    refactor: start service section changes

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,3 +1,4 @@
+import { services } from "../../index";
 
 function Service() {
     return (
@@ -7,38 +8,51 @@ function Service() {
             <h2 className="heading"><span>Our</span> Services</h2>
 
             <div className="services-container">
-                <div className="services-box barbing">
+
+                {services.map((item) => (
+                    <div className="services-box" key={item.id}>
+                        <img src={item.img} alt="" className="box-image" />
+
+                        <div className="box-texts">
+                            <h3> {item.title} </h3>
+                            <p> {item.description} </p>
+                            
+                            <a href="#" className="btn">Read More</a>
+                        </div>
+                    </div>
+                ))}
+
+                {/* <div className="services-box barbing">
                     <h3>Barbing</h3>
-                    <p>We render classic hair cuts to males bringing out the handsomeness in them. </p>
+                    <p> </p>
                     
                     <a href="#" className="btn">Read More</a>
                 </div>
 
                 <div className="services-box braiding">
                     <h3>Braiding</h3>
-                    <p>Get the beauty you desire in braids by our female hair stylists.</p>
+                    <p></p>
                     
                     <a href="#" className="btn">Read More</a>
                 </div>
 
                 <div className="services-box pedicure">
-                    <h3>Pedicure/Manicure</h3>
-                    <p>The neatness of your nails are also our priority. Our Pedicure and Manicure services is top-notch.</p>
+                    <h3></h3>
+                    <p></p>
                     
                     <a href="#" className="btn">Read More</a>
                 </div>
 
                 <div className="services-box tinting">
                     <h3>Tinting</h3>
-                    <p>All hair color tints are available at our salon. Our tints colors don't fade easily on hairs.</p>
+                    <p></p>
                     
                     <a href="#" className="btn">Read More</a>
                 </div>
 
                 <div className="services-box dreadlocks">
                     <h3>Dreadlocks</h3>
-                    <p>We give our fresh clients wonderful Dreadlocks and also relock and 
-                        beautify it during maintainance afterwards.
+                    <p>
                     </p>
 
                     <a href="#" className="btn">Read More</a>
@@ -46,10 +60,10 @@ function Service() {
 
                 <div className="services-box treatments">
                     <h3>Hair Treatments</h3>
-                    <p>We offer different types of hair treatments making use of our effective hair products.</p>
+                    <p></p>
 
                     <a href="#" className="btn">Read More</a>
-                </div>
+                </div> */}
             </div>
         </section>
     )
