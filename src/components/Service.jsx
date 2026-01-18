e45405feca687b63e6a15a167c3402fd53da6f65
Jonathan <jonathanafugwobi1@gmail.com>
2026-01-18 18:56:50 +0100

    Complete services section refactor

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -10,8 +10,7 @@ function Service() {
             <div className="services-container">
 
                 {services.map((item) => (
-                    <div className="services-box" key={item.id}>
-                        <img src={item.img} alt="" className="box-image" />
+                    <div className={`services-box ${item.name}`} key={item.id}>
 
                         <div className="box-texts">
                             <h3> {item.title} </h3>
@@ -21,49 +20,6 @@ function Service() {
                         </div>
                     </div>
                 ))}
-
-                {/* <div className="services-box barbing">
-                    <h3>Barbing</h3>
-                    <p> </p>
-                    
-                    <a href="#" className="btn">Read More</a>
-                </div>
-
-                <div className="services-box braiding">
-                    <h3>Braiding</h3>
-                    <p></p>
-                    
-                    <a href="#" className="btn">Read More</a>
-                </div>
-
-                <div className="services-box pedicure">
-                    <h3></h3>
-                    <p></p>
-                    
-                    <a href="#" className="btn">Read More</a>
-                </div>
-
-                <div className="services-box tinting">
-                    <h3>Tinting</h3>
-                    <p></p>
-                    
-                    <a href="#" className="btn">Read More</a>
-                </div>
-
-                <div className="services-box dreadlocks">
-                    <h3>Dreadlocks</h3>
-                    <p>
-                    </p>
-
-                    <a href="#" className="btn">Read More</a>
-                </div>
-
-                <div className="services-box treatments">
-                    <h3>Hair Treatments</h3>
-                    <p></p>
-
-                    <a href="#" className="btn">Read More</a>
-                </div> */}
             </div>
         </section>
     )
