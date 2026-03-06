ad6b39e4bb5eb8009ce4316718a8ab35e8f9b2ac
Jonathan <jonathanafugwobi1@gmail.com>
2026-03-06 19:39:01 +0100

    fix: correct wrong property names for Checkout map items

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,7 @@
 
 function Checkout({ cart }) {
+    console.log(cart);
+
     const total = cart.reduce(
         (acc, item) => acc + item.price * item.quantity,
             0
@@ -7,15 +9,21 @@ function Checkout({ cart }) {
 
     return (
         <div>
+            <h1>{JSON.stringify(cart)}</h1>
+            <h2> Checkout 🛒 </h2>
+
             {cart.map((item) => (
                 <div key={item.id}>
-                    <h3>{item.name}</h3>
+                    {console.log(item)}
+
+                    <h3>{item.title}</h3>
+                    <p>{item.description}</p>
                     <p>Quantity: {item.quantity}</p>
                     <p>Price: ${item.price * item.quantity}</p>
-
-                    <h2>Total: ${total}</h2>
                 </div>
             ))}
+
+            <h2>Total: ${total}</h2>
         </div>
     )
 }
